fa9a679ba29d83a5ba39a2a1ab5ec782c7e69a4b
thangphan37 <pcthangk59@gmail.com>
2020-10-30 14:22:00 +0900

    Add exercises/04-authentication

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,6 +6,11 @@ import React from 'react'
 // import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
+import * as auth from 'auth-provider'
 
 function App() {
   // 🐨 useState for the user
@@ -18,8 +23,61 @@ function App() {
 
   // 🐨 if there's a user, then render then AuthenitcatedApp with the user and logout
   // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
+  const {
+    data,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    setData,
+    run,
+  } = useAsync()
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => auth.logout().then(() => setData(null))
 
-  return <UnauthenticatedApp />
+  React.useEffect(() => {
+    async function getUser() {
+      const token = await auth.getToken()
+      const headers = token ? {Authorization: `Bearer ${token}`} : undefined
+      if (token) {
+        run(client('me', {headers}))
+      } else {
+        setData(null)
+      }
+    }
+
+    getUser()
+  }, [run, setData])
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  return data ? (
+    <AuthenticatedApp user={data} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
